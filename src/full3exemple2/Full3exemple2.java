24fd83a9026661fd0b74900302aef4ecef4a00a4
1r_DAM <1r_DAM@DESKTOP-SG5OKMH>
2022-11-22 18:02:35 +0100

    Revert "Tercer commit"
    This reverts commit ae94120697e00b442df29de826bdb0637b71287b.

--- src/full3exemple2/Full3exemple2.java
+++ src/full3exemple2/Full3exemple2.java
@@ -4,8 +4,7 @@ public class Full3exemple2 {
 
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("DOS");
-        System.out.println("3");
+        System.out.println("2");
     }
     
 }
